44f81f47b883fa1f3e27ba47f9ab9ff578227fc5
Mahi Shah <137817480+MahiShah30@users.noreply.github.com>
2024-01-28 21:15:13 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,19 +1,24 @@
 import hashlib
+
+#Global Variable
+malware_hashes= list(open("virusHash.txt","r").read().split('\n'))
+virusInfo= list(open("VirusInfo.txt","r").read().split('\n'))
+
 #Get Hash of file
-def md5_hash(filename):
+def sha256_hash(filename):
     with open(filename,"rb") as f: # rb--> open file in binary mode
         bytes=f.read()
-        md5Hash=hashlib.md5(bytes).hexdigest() # Convert file into hash 
+        md5Hash=hashlib.sha256(bytes).hexdigest() # Convert file into hash 
     return md5Hash
 #print(md5_hash("keylogger.zip"))
 
 #Malware Detection By Hash
 def malware_checker(pathOfFile):
-    hash_malware_check=md5_hash(pathOfFile)
-    counter=0 
+    global malware_hashes
+    global virusInfo
     
-    malware_hashes= list(open("virusHash.txt","r").read().split('\n'))
-    virusInfo= list(open("VirusInfo.txt","r").read().split('\n'))
+    hash_malware_check=sha256_hash(pathOfFile)
+    counter=0
 
     for i in malware_hashes:
         if i == hash_malware_check:
